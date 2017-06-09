e0c93d514181b9aad0622e6ce3bb14fe7d0c7822
beautilut <beautilut@gmail.com>
2017-06-09 14:18:02 +0800

    tijiao

---
+++ BTTOOL/UIView+EasyUse.m
@@ -0,0 +1,13 @@
+//
+//  UIView+EasyUse.m
+//  BTTOOLDemo
+//
+//  Created by Beautilut on 2017/6/9.
+//  Copyright © 2017年 beautilut. All rights reserved.
+//
+
+#import "UIView+EasyUse.h"
+
+@implementation UIView (EasyUse)
+
+@end

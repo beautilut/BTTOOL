59ac7bb5fce0a370f3cbab94f207f68b2d4ad7b3
beautilut <beautilut@gmail.com>
2017-06-09 16:15:24 +0800

    提交UIViewframe

---
+++ BTTOOL/UIView+Frame.h
@@ -0,0 +1,28 @@
+//
+// Created by Beautilut on 2017/6/9.
+// Copyright (c) 2017 beautilut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UIKit/UIKit.h"
+
+@interface UIView (Frame)
+
+@property (nonatomic) CGFloat left;
+
+@property (nonatomic) CGFloat right;
+
+@property (nonatomic) CGFloat top;
+
+@property (nonatomic) CGFloat bottom;
+
+@property (nonatomic) CGFloat width;
+
+@property (nonatomic) CGFloat height;
+
+@property (nonatomic) CGFloat centerX;
+
+@property (nonatomic) CGFloat centerY;
+
+
+@end
